f1de61b265585716d2355be0cedbb54c841e3fdf
Malak <mariarooy545@gmail.com>
2025-09-03 20:48:57 +0100

    commit

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,5 +1,13 @@
+// مسار الملف: alx-react-app/src/components/WelcomeMessage.jsx
+
 function WelcomeMessage() {
-  return <h2>Welcome to the app!</h2>;
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
 }
 
 export default WelcomeMessage;
