0bf242a9154ba83f19a9ae328bbe356806b8f1a2
Malak <mariarooy545@gmail.com>
2025-09-06 23:20:20 +0100

    commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,14 @@
-import UserDetails from "./UserDetails";
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
 
 function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
     <div>
       <h2>User Profile</h2>
-      <UserDetails />
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
