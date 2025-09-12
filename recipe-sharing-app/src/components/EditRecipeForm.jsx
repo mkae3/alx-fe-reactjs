59df32e4d61b220258eb7ec17fb04c4e0e6923af
Malak <mariarooy545@gmail.com>
2025-09-12 21:24:17 +0100

    commit

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -6,8 +6,8 @@ const EditRecipeForm = ({ recipe }) => {
   const [title, setTitle] = useState(recipe.title);
   const [description, setDescription] = useState(recipe.description);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     updateRecipe({ id: recipe.id, title, description });
     alert('Recipe updated!');
   };
@@ -20,12 +20,10 @@ const EditRecipeForm = ({ recipe }) => {
         value={title}
         onChange={(e) => setTitle(e.target.value)}
       />
-      <br />
       <textarea
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
-      <br />
       <button type="submit">Save Changes</button>
     </form>
   );
