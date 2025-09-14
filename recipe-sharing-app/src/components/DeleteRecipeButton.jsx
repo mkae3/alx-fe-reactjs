e9f4fd032f620eafe3617688e801d78939b4c831
Malak <mariarooy545@gmail.com>
2025-09-14 21:29:39 +0100

    commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,9 +1,17 @@
 import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
-  return <button onClick={() => deleteRecipe(recipeId)}>Delete</button>;
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+    navigate("/"); // بعد الحذف يرجع للهوم
+  };
+
+  return <button onClick={handleDelete}>Delete</button>;
 };
 
 export default DeleteRecipeButton;
+
