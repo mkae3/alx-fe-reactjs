a21f5d4354ac43e31bbf53cc5efcffc481b04595
Malak <mariarooy545@gmail.com>
2025-09-15 13:04:07 +0100

    commit

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ color: "navy", textAlign: "center" }}>
+        GitHub User Search
+      </h1>
+      <p style={{ textAlign: "center" }}>
+        Start building the search component here!
+      </p>
+    </div>
+  );
+}
+
+export default App;
