aaf64208ac06843b0097be6ad731bfa821c6751e
Malak <mariarooy545@gmail.com>
2025-10-05 21:25:10 +0100

    commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",            // ملف HTML الرئيسي
+    "./src/**/*.{js,jsx,ts,tsx}" // جميع ملفات React
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
