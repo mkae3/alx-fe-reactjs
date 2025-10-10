7e641bb454b6162d28c3bd800278700c01d2067e
Malak <mariarooy545@gmail.com>
2025-10-10 13:26:21 +0100

    commit

--- react-router-advanced/src/pages/Profile.jsx
+++ react-router-advanced/src/pages/Profile.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 
-const Profile = () => (
-  <div>
-    <h1>Profile Page</h1>
-    <nav>
-      <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
-    </nav>
-    <Outlet /> {/* Nested routes render here */}
-  </div>
-);
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <nav style={{ marginBottom: "1rem" }}>
+        <Link to="details" style={{ marginRight: "1rem" }}>
+          Details
+        </Link>
+        <Link to="settings">Settings</Link>
+      </nav>
+      {/* Nested routes will render here */}
+      <Outlet />
+    </div>
+  );
+};
 
 export default Profile;
